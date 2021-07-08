660f90627a9e758b2510d98f4d3a5c40e387f2e4
dbwogus94 <1994dbwogus@gmail.com>
2021-07-09 03:35:45 +0900

    <chore>  Correct a comment
    주석 설명 오류 수정

--- dreamCodeing/node/4-node-module/4-module/app.js
+++ dreamCodeing/node/4-node-module/4-module/app.js
@@ -1,16 +1,16 @@
 /**
  * ### node에서 제공하는 export모듈과 import모듈 사용하기
- * 2015년 es6부터 제공되는 기능이다.
- * 1. 모듈 export 하기 :  .js파일에서 module.exports를 사용하여 export 하기 
+ * -> node.js는 기본적으로 CommonJS의 모듈 시스템을 사용한다.
+ * 1. 모듈 export 하기 :  .js파일에서 module.exports를 사용하여 export 하기
  * 2. 모듈 import 하기 : require('${파일명.js}')를 사용하여  모듈 import 하기
  */
 
 /* require를 통해 export된 모듈을 import 할 수 있다.
-    **주의점! import를 할때 counter.js에 선언된 log가 출력된다.
-*/ 
+ **주의점! import를 할때 counter.js에 선언된 log가 출력된다.
+ */
 const counter = require('./counter.js');
 
 counter.increase();
 counter.increase();
 counter.increase();
-console.log("getCount : " + counter.getCount());
+console.log('getCount : ' + counter.getCount());
--- dreamCodeing/node/4-node-module/5-module/app.js
+++ dreamCodeing/node/4-node-module/5-module/app.js
@@ -1,11 +1,12 @@
 /**
  * ### javaScript에서 제공하는 import export 사용하기
+ *  → es6부터 제공하는 js공식 모듈이다.
  *  1. package.json 생성 : mpn을 사용하여 모듈로 사용될 위치에 package.json 생성하기
  *    >> 명령어 : npm init --yes
  *  2. package.json 수정 : 값을 추가해 모듈로 사용 설정
  *    >> "type": "module",
  *  3. export할 함수 지정 : 함수명에 export 명령어를 추가한다.
- *  4. import 하기 
+ *  4. import 하기
  */
 
 /* 1. 사용할 함수별로 import */
@@ -15,11 +16,10 @@
 // increase();
 // console.log("getCount : " + getCount());
 
-
 /* 2.모듈 전체를 변수에 import */
 import * as counter from './counter.js';
 
 counter.increase();
 counter.increase();
 counter.increase();
-console.log("getCount : " + counter.getCount());
+console.log('getCount : ' + counter.getCount());
