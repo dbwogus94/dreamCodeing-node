ba1a8e80e0fe7dd37019d4638c8790fc57426f13
dbwogus94 <1994dbwogus@gmail.com>
2021-07-27 02:11:44 +0900

    <feat>(validation) : Tweet Service Validation
    # express-validator npm install
    
    # validator/tweetValidator.js 추가
     - post :  post로 받은 body에 대해 validator와 sanitization 수행
     - put : put으로 받은 body에 대해 validator 수행
    
    # 선언한 validator은 라우트에서 사용한다.
    - validator은 빠르면 빠를 수록 비용을 절약할 수 있기 때문에
      가장 빠른 라우트에서 수행하도록 적용하였음

--- dreamCodeing/projects/Dwitter/server/src/routes/tweetRouter.js
+++ dreamCodeing/projects/Dwitter/server/src/routes/tweetRouter.js
@@ -2,6 +2,9 @@ import express from 'express';
 // 비동기 에러를 잡는 미들웨어 라우터 마다 선언해야한다.
 import 'express-async-errors';
 import * as tweetController from '../controllers/tweetController.js';
+// 유효성 검증 모듈
+import { postValidator, putValidator } from '../validator/tweetValidator.js';
+
 /**
  * ### View
  * - 라우터는 MVC에서 View에 대한 책임이 있다.
@@ -16,9 +19,9 @@ router.get('/', tweetController.getTweets);
 // GET /tweets/:id
 router.get('/:id', tweetController.getTweet);
 // POST /tweets/:id
-router.post('/', tweetController.createTweet);
+router.post('/', postValidator, tweetController.createTweet);
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', putValidator, tweetController.updateTweet);
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
 
