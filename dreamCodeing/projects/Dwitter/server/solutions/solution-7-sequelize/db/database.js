f841acf6e3d6a6a8e6c501f0b522339ddddeabb6
jae hyun <1994dbwogus@gmail.com>
2021-10-01 18:19:47 +0900

    refactor(solution-7):  Deprecate Sql code
    # db/database.js
    - mysql createPool 관련 코드 제거

--- dreamCodeing/projects/Dwitter/server/solutions/solution-7-sequelize/db/database.js
+++ dreamCodeing/projects/Dwitter/server/solutions/solution-7-sequelize/db/database.js
@@ -1,4 +1,3 @@
-import mysql from 'mysql2';
 import { config } from '../config.js';
 import SQ from 'sequelize';
 
@@ -7,15 +6,5 @@ const { host, user, password, database } = config.mysql;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
   dialect: 'mysql',
+  logging: true, // default(true), sql 실행 로그 사용 유무
 });
-
-// DB pool 생성  -> TODO: sequelize 적용완료시 제거
-const pool = mysql.createPool({
-  host: config.mysql.host,
-  user: config.mysql.user,
-  database: config.mysql.database,
-  password: config.mysql.password,
-});
-
-// 비동기 작업을 위해 생성된 pool이 가진 promise()를 내보낸다.
-export const db = pool.promise();
