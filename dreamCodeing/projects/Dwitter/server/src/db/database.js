11cb6c82e0b133aef3231aa632394784a14240f1
jae hyun <1994dbwogus@gmail.com>
2021-10-18 18:46:58 +0900

    refactor(my-mongoDb):  Refactoring db/database.js
    # dropCollection 함수 refactoring
    - "함수 추출하기 (Extract Function)"방법으로 리팩터링
    
    # getCollections 신규 정의
    - dropCollection의 내부로직을 추출하여 getCollections함수로 신규 정의

--- dreamCodeing/projects/Dwitter/server/src/db/database.js
+++ dreamCodeing/projects/Dwitter/server/src/db/database.js
@@ -1,5 +1,6 @@
 import { config } from '../config/config.js';
 import { MongoClient } from 'mongodb';
+import moment from 'moment';
 
 /**
  * MongoDB connection
@@ -14,8 +15,43 @@ export async function connectDB() {
 /**
  * 존재하는 collection 모두 drop
  * @param {Db} db - Db instance
+ * @throws fail drop collections
  */
 export async function dropCollection(db) {
+  const collections = await getCollections(db);
+  const results = await asyncDrop(collections);
+
+  if (!results.includes(false)) {
+    printDropLog(collections);
+  } else {
+    throw new Error('fail drop collections');
+    // TODO: MongoDB 트랜잭션 적용하여 모두 성공하지 않은 경우 롤백과 예외 던지도록 해야한다.
+  }
+
+  async function asyncDrop(collections) {
+    // Promise.all을 사용하여 collcetion drop 일괄 요청
+    const results = await Promise.all(
+      collections.map(collection => {
+        return collection.drop();
+      })
+    );
+    return results;
+  }
+
+  function printDropLog(collections) {
+    collections.forEach(collection => {
+      //console.info(`[${collection.s.namespace.db}] Drop '${collection.s.namespace.collection}' Collection`);
+      console.info(`[${moment().format('yyyy-MM-DD HH:mm:ss')}] Drop Collection '${collection.namespace}'`);
+    });
+  }
+}
+
+/**
+ * get database collections
+ * @param {Db} db
+ * @returns collections
+ */
+export async function getCollections(db) {
   /*db.listCollections() : db에 존재하는 모든 collection 정보를 가진 커서 ListCollectionsCursor를 가져온다.
     - ListCollectionsCursor에서 얻어온 collection은 실제 컬렉션 객체(db.collection()) 과 다르다
     - ListCollectionsCursor에서 얻어온 collection은 컬렉션 정보를 가진 object를 내보낸다.
@@ -24,16 +60,5 @@ export async function dropCollection(db) {
   */
   const collections = await db.listCollections().toArray();
   collections.filter(collection => collection.type === 'collection');
-  // drop 요청
-  await Promise.all(
-    collections.map(collection => {
-      return db.collection(collection.name).drop();
-    })
-  );
-  // 결과 log,
-  collections.forEach(collection => {
-    console.info(`[${db.s.namespace.db}] Drop '${collection.name}' Collection`);
-  });
-
-  // TODO: MongoDB 트랜잭션 적용하여 모두 성공하지 않은 경우 롤백과 예외 던지도록 해야한다.
+  return collections.map(collection => db.collection(collection.name));
 }
