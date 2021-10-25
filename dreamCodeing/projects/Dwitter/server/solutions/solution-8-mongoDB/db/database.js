32b50db1313dcebbc172cbec3d99aa6f87bc5182
jae hyun <1994dbwogus@gmail.com>
2021-10-25 17:16:08 +0900

    feat(solution-8-mongoDB): Add get Collection code
    # db/database.js
    - mongoDB와 연결을 책임지는 모듈이다.
    - 때문에 앱에서 사용할 Collection을 가져오는 코드를 추가 한다.
    
    ## getUsers
    - db에서 users collection 정보를 가져와 Collection 클래스의 인스턴스로 리턴한다.
    
    ## getTweets
    - db에서 tweets collection 정보를 가져와 Collection 클래스의 인스턴스로 리턴한다.

--- dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/db/database.js
+++ dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/db/database.js
@@ -1,13 +1,33 @@
 import { config } from '../config.js';
 import MongoDb from 'mongodb';
 
+/** @type {MongoDb.Db} */
+let db; // db 접속정보를 담을 모듈 전역변수
+
 /**
  * Connect mongoDB
  * @returns Db class instance
  * - Db class : MongoDB 데이터베이스 클래스
  */
 export async function connectDB() {
-  // MongoClient를 통해 client가 가진 db를 받아온다.
   return MongoDb.MongoClient.connect(config.mongoDB.host) //
-    .then(client => client.db());
+    .then(client => {
+      db = client.db();
+    });
+}
+
+/**
+ * get users collection
+ * @returns Collection instance
+ */
+export function getUsers() {
+  return db.collection('users');
+}
+
+/**
+ * get tweets collection
+ * @returns Collection instance
+ */
+export function getTweets() {
+  return db.collection('tweets');
 }
