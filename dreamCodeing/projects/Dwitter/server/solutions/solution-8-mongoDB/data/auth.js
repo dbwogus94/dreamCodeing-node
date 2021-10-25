d0076d756dfa767d1dc7edabb5ccbfabe92d04fc
jae hyun <1994dbwogus@gmail.com>
2021-10-25 17:28:48 +0900

    feat(solution-8-mongoDB): Repository Change DB
    # data/auth.js MongoDB를 사용하도록 코드 변경
    
    # mapOptionalUser() 신규 정의
    - mongoDB의 식별자 변수는 _id를 사용한다.
    - 서비스에서 사용되는 user의 식별자는 id를 사용한다.
    - 즉, DB의 변경으로 인한 포멧의 차이가 발생한다.
    - 이러한 DB변경에 따른 포멧의 차이는 data 계층에서 맞춰 리턴해야 한다.
    - 즉, 함수 mapOptionalUser를 새로 정의하여 포맷의 차이를 맞추는데 사용하였다.

--- dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/data/auth.js
+++ dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/data/auth.js
@@ -1,33 +1,35 @@
-export const users = [
-  {
-    id: '1', // DB에서 id를 부여한다.
-    username: 'bob',
-    password: '$2b$12$mhnJ92tY/OyL0C63tEkrcuU9REMXU5t0FloNQFc1m0R3wl2nLI9DW', // 12345
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: '',
-  },
-  {
-    id: '2', // DB에서 id를 부여한다.
-    username: 'jay',
-    password: '$2b$12$mhnJ92tY/OyL0C63tEkrcuU9REMXU5t0FloNQFc1m0R3wl2nLI9DW', // 12345
-    name: 'Jay',
-    email: 'jay@gmail.com',
-    url: '',
-  },
-];
+import { getUsers } from '../db/database.js';
+import MongoDb from 'mongodb';
+const ObjectId = MongoDb.ObjectId;
 
 export async function findByUsername(username) {
-  return users.find(user => user.username === username);
-  // 없으면 undefined
+  return getUsers() //
+    .findOne({ username })
+    .then(mapOptionalUser);
 }
+
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  return getUsers()
+    .insertOne(user)
+    .then(data => data.insertedId.toString());
 }
 
 export async function findById(id) {
-  return users.find(user => user.id === id);
-  // 없으면 undefined
+  return getUsers()
+    .findOne({ _id: new ObjectId(id) })
+    .then(mapOptionalUser);
+}
+
+/*### 변수명 이유  
+  - map : a를 b로 변경한 값을 리턴하는 연산이다
+  - Optional :  필수가 아니다. 즉 값이 null일 수 있다를 의미한다.
+  - User : 리턴할 데이터 의미
+  - 즉, 
+    user를 받아 새로운 객체로 리턴하는 함수이며, 
+    결과는 null일 수 있다.
+*/
+function mapOptionalUser(user) {
+  return user
+    ? { ...user, id: user._id } // 서비스에 필요한 데이터 형식으로 변경
+    : user; // null
 }
