0e38717929ccdcdd8920072e474a40833f9b79d4
jae hyun <1994dbwogus@gmail.com>
2021-10-25 19:58:27 +0900

    feat(solution-8-mongoDB): Repository Change DB
    # data/tweet.js MongoDB를 사용하도록 코드 변경
    
    # mongodb에서 조회된 data를 가공하는 함수 정의
    -  mapOptionalTweet 신규 정의
    - mapTweets 신규 정의
    - DB와 서비스에서 사용하는 데이터를 맞추기 위한 가공 함수이다.

--- dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/data/tweet.js
+++ dreamCodeing/projects/Dwitter/server/solutions/solution-8-mongoDB/data/tweet.js
@@ -1,136 +1,154 @@
-/**
- * ### Model 계층 : DATA에 관한 보관, CRUD, 가공을 담당한다.
- * - data(리소스) 조작(CURD)과 가공을 담당하는 계층이다.
- * - Spring으로 생각하면 service계층과 DAO 계층이 여기에 속한다.
- *
- * ### 리펙터링
- * 1단계) data를 다루는 코드는 data 계층으로
- *  기존 tweets.js에 있던 data를 조작하는 로직을 해당 data 계층으로 이동시킨다.
- * 2단계) 함수명에서 중복되는 이름을 제거한다.
- *  data 계층을 사용하는 곳에서 이름을 tweetRepository로 사용한다
- *  그렇기 때문에 함수명에 tweet에 들어가지 않게 명명한다.
- *
- * ### solution-4-jwt에서 수정
- * 유저의 정보는 data/auth.js에서 관리하기 한다.
- * 그렇기 때문에 tweets에 있던 유저의 정보를 userId로 변경하고
- * userId를 통해 유저의 정보에 접근하도록 변경한다.
- *
- * 아래 함수 모두 userId를 통해 user의 정보에 접근하도록 변경
- * - getAll()
- * - getAllByUsername
- * - getById()
- * - create()
- * - update()
- */
-
 import * as userRepository from '../data/auth.js';
+import { getTweets } from '../db/database.js';
+import { ObjectId } from 'mongodb';
 
-let tweets = [
-  {
-    id: '1',
-    text: '솔루션 코드는 재밌습니다.',
-    createAt: new Date().toString(),
-    userId: '1', // userId를 통해 유저의 정보에 접근
-  },
-  {
-    id: '2',
-    text: 'Hi~',
-    createAt: new Date().toString(),
-    userId: '1',
-  },
-];
-
-/**
- * 유저의 정보를 담은 전체 tweet을 가져온다.
- * @returns
- * tweets = [
- *  { tweet, usernaem, name, url },
- *   ...
- * ]
+/** ## NOSQL (정보의 중복 > 관계)
+ *  - NOSQL은 RDB에 비해 조인쿼리의 성능이 많이 떨어진다
+ *  - 때문에 NOSQL은 관계를 가지는 것보다 중복을 허용하는 쪽을 택하는 경우가 많다.
+ *
+ *  ### 모든 사용자가 트윗을 쿼리하는 횟수 vs 사용자가 사용자의 정보를 업데이트 하는 횟수
+ *
+ *  1) 모든 사용자가 트윗을 쿼리하는 횟수 > 사용자가 사용자의 정보를 업데이트 하는 횟수
+ *   -> dwitter 프로젝트 특성을 보면 트윗을 빠르게 CRUD하는 것이 주 목적이다.
+ *    이러한 서비스에 document간에 관계를 가지는 것은 너무나 비효율적이다.
+ *    때문에 tweet document에서 필요한 유저의 정보를 중복하여,
+ *    가지게 하는 방법을 많이 사용한다.
+ *   -> 만약 user의 정보가 변경된다면?
+ *    즉각 반영해야하는 서비스라면 바로 모든 tweets를 돌면서 update를 실행한다.
+ *    즉각 반영하지 않아도 되는 서비스라면 스케줄링같은 기술을 사용하여 트래픽이 적은 시간에 처리한다.
+ *
+ *  2) 모든 사용자가 트윗을 쿼리하는 횟수 < 사용자가 사용자의 정보를 업데이트 하는 횟수
+ *   -> 서비스 특성상 사용자의 정보가 빈번하게 변경되고,
+ *    그 데이터를 즉각 반영해야 한다면 관계를 가지는게 나을 수도 있다.
+ *    이 경우 조금 느린 조회 성능을 감수하고 NoSQL 관계를 가지던가,
+ *    RDB를 사용하는 것이 더 나은 방법일 수도 있다.
  */
+
+// let tweets = [
+//   {
+//     id: '1',
+//     text: '솔루션 코드는 재밌습니다.',
+//     createAt: new Date().toString(),
+//     userId: '1', // userId를 통해 유저의 정보에 접근
+//   },
+//   {
+//     id: '2',
+//     text: 'Hi~',
+//     createAt: new Date().toString(),
+//     userId: '1',
+//   },
+// ];
+
 export async function getAll() {
-  return Promise.all(
-    tweets.map(async tweet => {
-      const { username, name, url } = await userRepository.findById(tweet.userId);
-      return { ...tweet, username, name, url };
-      // async를 사용했기 때문에 promise를 리턴한다.
-      // 그렇기 때문에 Promise.all의 인자로 사용이 가능하다.
-    })
-  );
+  return getTweets() //
+    .find()
+    .sort({ createAt: -1 }) // -1: 내림차순
+    .toArray()
+    .then(mapTweets);
 }
-/**
- * username(작성자)와 일치하는 모든 tweet 조회
- * @param {string} username
- * @returns
- * tweets = [
- *  { tweet, usernaem, name, url },
- *   ...
- * ]
- */
+
 export async function getAllByUsername(username) {
-  // username는 userRepository에서 가지고 있다. 그렇기 때문에 getAll 호출
-  const tweets = await getAll();
-  return tweets.filter(tweet => tweet.username === username);
+  return getTweets() //
+    .find({ username })
+    .sort({ createdAt: -1 })
+    .toArray()
+    .then(mapTweets);
 }
-/**
- * tweet의 id와 일치하는 tweet 조회
- * @param {string} id
- * @returns
- * tweet = { tweet, username, name, url }
- */
+
 export async function getById(id) {
-  // 1) tweets에서 인자로 받은 id와 일치하는 tweet 찾기
-  const found = tweets.find(tweet => tweet.id === id); // 없으면 undefined
-  if (!found) {
-    return null;
-  }
-  // 2) 찾은 tweet에서 userId를 통해 유저의 정보를 찾기
-  const { username, name, url } = await userRepository.findById(found.userId);
-  // 3) 찾은 tweet과 user의 정보를 합하여 리턴한다.
-  return { ...found, username, name, url };
+  return getTweets()
+    .findOne({ _id: new ObjectId(id) })
+    .then(mapOptionalTweet);
 }
-/**
- * 신규 tweet 생성
- * @param {string} text
- * @param {string} userId
- * @returns
- * tweet = {tweet, username, name, url}
- */
+
 export async function create(text, userId) {
-  // 1) 트윗 생성
+  const { name, username, url } = await userRepository.findById(userId);
   const tweet = {
-    id: Date.now().toString(),
     text,
-    createAt: new Date().toString(),
+    createdAt: new Date(),
     userId,
+    name: name,
+    username: username,
+    url: url,
   };
-  // 2) 기존 tweets 가장 앞에 신규 트윗 추가
-  tweets = [tweet, ...tweets];
-  // 3) user정보가 포함된 tweet을 리턴한다.
-  return getById(tweet.id);
+
+  return getTweets() //
+    .insertOne(tweet)
+    .then(data => mapOptionalTweet({ ...tweet, _id: data.insertedId }));
 }
-/**
- * tweet의 id와 일치하는 tweet 수정
- * @param {string} id
- * @param {string} text
- * @returns
- * tweet = {tweet, username, name, url}
- */
+
 export async function update(id, text) {
-  // 1) 일치하는 트윗 찾기
-  const tweet = tweets.find(tweet => tweet.id === id);
-  if (!tweet) {
-    return null;
-  }
-  // 2) tweet 수정
-  tweet.text = text;
-  // 3) user의 정보를 포함한 tweet을 리턴
-  return getById(id);
+  // updataOne()는 update이후 수정된 값을 리턴하지 않는다.
+  // 그래서 API에서는 리턴 값이 필요한 경우 findOneAndUpdate()를 사용하도록 권고한다.
+  return getTweets() //
+    .findOneAndUpdate(
+      { _id: new ObjectId(id) }, // 찾을 document
+      { $set: { text } }, // 수정할 내용
+      { returnDocument: 'after' } // 수정 후 결과를 문서를 리턴하도록 설정
+      /* findOneAndUpdate()의 returnDocument 옵션
+        - findOneAndUpdate는 기본적으로 수정하기 전 document를 리턴한다.
+        - 하지만 returnDocument에 'after'를 값으로 지정하면 수정후 document를 리턴한다.
+      */
+    )
+    .then(result => result.value)
+    .then(mapOptionalTweet);
 }
-/**
- * tweet의 id와 일치하는 트윗 삭제
- * @param {string} id
- */
+
 export async function remove(id) {
-  tweets = tweets.filter(tweet => tweet.id !== id);
+  return getTweets().deleteOne({ _id: new ObjectId(id) });
 }
+
+/**
+ * service나 client에서 사용하는 데이터 형식으로 tweet을 변경한다.
+ * @param {object} tweet
+ * @returns tweet || null
+ * - tweet: { id, text, createdAt, userId, username, name, url }
+ * - null: 자원 없음
+ */
+function mapOptionalTweet(tweet) {
+  return tweet //
+    ? { ...tweet, id: tweet._id.toString() }
+    : tweet; // null
+}
+
+/**
+ * service나 client에서 사용하는 데이터 형식으로 tweets을 변경한다.
+ * @param {array} tweets
+ * @returns tweets || []
+ * - tweets: {tweet, tweet, ...}
+ * - tweet: { id, text, createdAt, userId, username, name, url }
+ */
+function mapTweets(tweets) {
+  return tweets.map(mapOptionalTweet);
+}
+
+/*Q) mapOptionalTweet와 mapTweets를 정의하여 사용하는 이유
+  A) Repository는 서비스에서 사용하는 data를 조회 가공하는 책임을 가지기 때문이다.
+
+  ### 데이터 가공이 필요한 이유
+  조건1. mongoDB는 DB 특성에 따라서 고유 식별자를 _id로 가진다.
+  조건2. dwitter에서는 문자열 id를 식별자로 사용한다.
+  - 위처럼 "DB에서 사용하는 데이터"와 "앱에서 사용하는 데이터" 간에 차이가 발생하면,
+  - repository에서 필요한 데이터를 추가로 만들어주는 가공의 작업이 들어간다.
+
+  Q) 그렇다면 _id는 서비스에서 사용하지 않는데 왜 제거하지 않을까?
+  - 사실 앱에서 _id를 사용하지 않기 때문에 repository에서 제거하여 service로 리턴해도 된다.
+  - 하지만 service로직에서 _id를 통한 작업을 할 수 있기 때문에 편의상 제거하지 않는 경우도 많다.
+  
+  ### 설계에 따라 service에서 데이터를 한번 더 가공하여 controller에게 전달하기도 한다.
+  ex) Spring은 DTO, Entity를 구분하여 사용한다.
+  - Service <-> DAO(Repository)
+    => 두 계층 간에는 Entity를 사용하여 데이터를 주고 받는다.
+
+  - Controller <-> Service
+    => 두 계층 간에는 DTO를 사용하여 데이터를 주고 받는다.
+    => 최종적으로 client에는 DTO의 데이터가 json으로 파싱되어 전달된다.
+  
+  Q) 왜 하나의 포멧을 사용하지 않고 불편하게 가공의 작업을 할까?
+  A) 그것은 repository에는 절대 원칙이 있기 때문이다.
+
+  ### repository에는 절대 원칙.
+  1) 데이터베이스에서 필요한 데이터만 읽어 온다
+  2) 불필요한 데이터가 불가피하게 포함되어 있다면 읽어온 데이터를
+    백엔드/프롣트엔드에 필요한(앱에서 필요한)데이터 모델로 변경한다.
+*/
